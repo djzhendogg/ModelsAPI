c56d78fd03656d3aac2a743d3a6f47b39d356c6a
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2025-03-31 13:28:28 +0300

    apt_mol1.3

--- apt_prot/service.py
+++ apt_prot/service.py
@@ -44,4 +44,7 @@ def predict(apt: str, prot: str) -> int:
     X_selected = selection.transform(full_df)
 
     X_poly = poly.transform(X_selected)
+    ans = best_model.predict(X_poly)
+    print("РЕЗУЛЬТАТ МОДЕЛИ")
+    print(ans)
     return best_model.predict(X_poly)
