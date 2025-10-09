b90bca7c26801d0a6afaf4881bdbc0d2f54c3af0
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-10-09 16:08:27 +0300

    update config

--- link_prediction/config.py
+++ link_prediction/config.py
@@ -3,12 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ENTITY_JSON = "entity_names_molecules_0.json"
-EMB_H5      = "embeddings_molecules_0.v48.h5"
-MODEL_H5    = "model.v48.h5"
-# PROJECT_PATH = os.getenv('PROJECT_PATH')
-# MODEL_PATH = os.getenv('MODEL_PATH')
-
-DIM         = 400
-TOPK        = 100
-COMPARATOR_TYPE  = "cos"
+ENTITY_JSON = os.getenv('ENTITY_JSON')
+EMB_H5 = os.getenv('EMB_H5')
+MODEL_H5 = os.getenv('MODEL_H5')
+DIM = 400
+TOPK = 100
+COMPARATOR_TYPE = "cos"
